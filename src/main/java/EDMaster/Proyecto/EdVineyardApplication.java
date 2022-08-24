6c2063dbf685cf26955dd6ba34e37335cd2ccdf7
CifuentesJ <juliancifuentesvasquez@gmail.com>
2022-08-23 21:24:40 -0500

    Creación plantilla spring boot

---
+++ src/main/java/EDMaster/Proyecto/EdVineyardApplication.java
@@ -0,0 +1,13 @@
+package EDMaster.Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdVineyardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdVineyardApplication.class, args);
+	}
+
+}
