8145d5c8bd9142867c626016731e9e7ec07b0fa8
CifuentesJ <juliancifuentesvasquez@gmail.com>
2022-08-25 23:16:16 -0500

    Continuación en el trabajo de la clase empleado, trabajo con la clase movDinero

---
+++ src/main/java/EDMaster/Proyecto/Empleado.java
@@ -0,0 +1,66 @@
+package EDMaster.Proyecto;
+
+import java.util.Arrays;
+
+public class Empleado {
+    private String  email, rol;
+    private int id;
+    private Perfil perfil;
+    private MovimientoDinero[] movimientoDineros;
+    
+
+    public Empleado(int id, String email, Perfil perfil, String rol) {
+        this.id = id;
+        this.email = email;
+        this.perfil = perfil;
+        this.rol = rol;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Perfil getPerfil() {
+        return perfil;
+    }
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+    public MovimientoDinero[] getMovimientoDineros() {
+        return movimientoDineros;
+    }
+    public void setMovimientoDineros(MovimientoDinero[] movimientoDineros) {
+        if(this.rol.equalsIgnoreCase("administrador")){
+            this.movimientoDineros = movimientoDineros;
+        }else{
+            System.out.println("Usted no es administrador, no tienes permiso.");
+        }
+    }
+    public String getRol() {
+        return rol;
+    }
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+
+    public void CambiarNombre(String cambiarNombre) {
+        this.perfil.setId(cambiarNombre);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Empleado [email=" + email + ", id=" + id + ", movimientoDineros=" + Arrays.toString(movimientoDineros)
+                + ", " + perfil.toString() + ", rol=" + rol + "]";
+    }
+
+    
+    
+}
---
+++ src/main/java/EDMaster/Proyecto/Perfil.java
@@ -0,0 +1,45 @@
+package EDMaster.Proyecto;
+
+
+public class Perfil{
+    //Atributos para la clase. El atributo user debemos saber cómo implementarlo
+    private String id, imagen, telefono; 
+    
+
+    //Constructor para la clase
+    public Perfil(String id, String imagen, String telefono) {
+        this.id = id;
+        this.imagen = imagen;
+        this.telefono = telefono;
+    }
+
+    //Getters y setters para la clase
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getimagen() {
+        return imagen;
+    }
+    public void setimagen(String imagen) {
+        this.imagen = imagen;
+    }
+    public String gettelefono() {
+        return telefono;
+    }
+    public void settelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Perfil = [id=" + id + ", imagen=" + imagen + ", telefono=" + telefono + "]";
+    }
+    
+    
+
+    //Aún no he creado el "createdAt" ni "updatedAt", no sé si son métodos o atributos
+    
+}
