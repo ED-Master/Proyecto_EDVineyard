8c8b5fa0488ac4c265f8c98d65a17b426cfb516f
EduarBarrios <edu3.barrios@gmail.com>
2022-08-25 22:14:47 -0500

    Trabajando en la clase MovimientoDinero

--- src/main/java/EDMaster/Proyecto/EdVineyardApplication.java
+++ src/main/java/EDMaster/Proyecto/EdVineyardApplication.java
@@ -8,6 +8,19 @@ public class EdVineyardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdVineyardApplication.class, args);
+
+		MovimientoDinero M1=new MovimientoDinero(100000,"Compra de insumos",-200000);
+		System.out.println(M1.getMonto());
+		M1.setMonto(-250000);
+
+        MovimientoDinero Venta=new MovimientoDinero(123245,"Venta de pro",1000000);
+          Venta.setConcepto("Venta despues de Iva");
+		  Venta.setMonto((float) (Venta.getMonto()*0.81));
+		System.out.println(Venta.getMonto());
 	}
+
+
+
+
 
 }
