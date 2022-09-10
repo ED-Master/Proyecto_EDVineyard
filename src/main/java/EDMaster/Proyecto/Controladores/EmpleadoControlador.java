8b55de5bc32d6a38ff3673a48c6da81bf8d46d46
pau77cc <pau77cc@gmail.com>
2022-09-10 18:17:09 -0500

    Se raliza el servicio y el controlador para la clase empleado

--- src/main/java/EDMaster/Proyecto/Controladores/EmpleadoControlador.java
+++ src/main/java/EDMaster/Proyecto/Controladores/EmpleadoControlador.java
@@ -1,10 +1,49 @@
 package EDMaster.Proyecto.Controladores;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Optional;
+import EDMaster.Proyecto.Entidades.Empleado;
 import EDMaster.Proyecto.Servicios.EmpleadoServicio;
 
 @RestController
 public class EmpleadoControlador {
-    private EmpleadoServicio servicio;
+    private EmpleadoServicio servicios;
+
+
+    public EmpleadoControlador(EmpleadoServicio servicios){
+        this.servicios = servicios;        
+    }
+
+    @GetMapping("/employees")
+    public ArrayList<Empleado> listarEmpleados(){
+        return this.servicios.listarEmpleados();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Optional<Empleado> buscarEmpleado(@PathVariable("id") Long id){
+        return this.servicios.buscarEmpleado(id);       
+    }
+
+    @PostMapping("/employees")
+    public String crearEmpleado(@RequestBody Empleado empleado){
+        return this.servicios.crearEmpleado(empleado);
+    } 
+
+    @PatchMapping("/employees/{id}")
+    public Empleado actualizarCampo(@PathVariable("id") Long id, @RequestBody Map<Object, Object> mapeoEmpleado){
+        return this.actualizarCampo(id, mapeoEmpleado);        
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String eliminarEmpleado(@PathVariable Long id){
+        return this.servicios.eliminarEmpleado(id);
+    }
 }
--- src/main/java/EDMaster/Proyecto/Servicios/EmpleadoServicio.java
+++ src/main/java/EDMaster/Proyecto/Servicios/EmpleadoServicio.java
@@ -1,10 +1,61 @@
 package EDMaster.Proyecto.Servicios;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
-import EDMaster.Proyecto.Repositorio.EmpleadoRepositorio;
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
 
+import EDMaster.Proyecto.Repositorio.EmpleadoRepositorio;
+import net.bytebuddy.asm.Advice.Return;
+import EDMaster.Proyecto.Entidades.Empleado;
+import EDMaster.Proyecto.Entidades.Empresa;
 @Service
 public class EmpleadoServicio {
     private EmpleadoRepositorio repositorio;
+
+    public EmpleadoServicio (EmpleadoRepositorio repositorio) {
+        this.repositorio = repositorio;    
+    }
+
+    public ArrayList<Empleado> listarEmpleados(){
+        return (ArrayList<Empleado>) this.repositorio.findAll();
+    }
+
+    public Optional<Empleado> buscarEmpleado(long id){
+        return this.repositorio.findById(id);
+    }
+
+    public String crearEmpleado(Empleado empleado){
+        if (this.repositorio.findById(empleado.getId()).isEmpty()){
+            this.repositorio.save(empleado);
+            return "Se crea el empleado exitosamente";             
+        }else{
+            return "ya existe un empleado con ese Id";    
+        }
+    }
+
+    public Empleado actualizarCampo(long id, Map<Objet, Objet> empleadoMapeo){
+        Empleado empleado = this.repositorio.findById(id).get();
+
+        empleadoMapeo.forEach((key,Value)->{
+            Field campo = ReflectionUtils.findField(Empleado.class, (String) key);
+            campo.setAccessible(true);
+            ReflectionUtils.setField(campo, empleado, value);            
+        });
+        return this.repositorio.save(empleado);
+    }
+
+    public String eliminarEmpleado(long id){
+        if (this.repositorio.findById(id).isPresent()){
+            this.repositorio.deleteById(id);
+            return "Se eliminó exitosamente el empleado";
+        }else{
+            return "No existe un empleado con tal Id para ser eliminado",
+        }
+    }
 }
+
