1508647f14493f2b80ea52e39c7c6602d2f14b38
santy0430 <sandarriaga@hotmail.com>
2022-09-10 23:40:47 -0500

    Se actualizan Servicios

--- src/main/java/EDMaster/Proyecto/Servicios/serviciosMD.java
+++ src/main/java/EDMaster/Proyecto/Servicios/serviciosMD.java
@@ -1,14 +1,15 @@
 package EDMaster.Proyecto.Servicios;
 
 
-import EDMaster.Proyecto.Entidades.Empleado;
 import EDMaster.Proyecto.Entidades.MovimientoDinero;
-import EDMaster.Proyecto.Entidades.Perfil;
 import EDMaster.Proyecto.Repositorio.MD_Repositorios;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class serviciosMD {
@@ -26,23 +27,42 @@ public class serviciosMD {
 
 
 
-    public MovimientoDinero BuscarP(int index) {
-        return repositorio.get(index);
+    public Optional<MovimientoDinero> BuscarP(int index) {
+        return repositorio.findById(index);
     }
 
     public String crearMD(MovimientoDinero x) {
-        repositorio.add(x);
-        return "Movimiento Registrado";
+        if (repositorio.findById(x.getId()).isEmpty()){
+            repositorio.save(x);
+            return"Se crea el movimiento exitosamente";
+
+        }
+        else{
+            return"No se creo el movimiento";
+        }
     }
 
     public String eliminarMovimiento(int index) {
-        repositorio.remove(index);
-        return "Movimiento Eliminado";
+        if (repositorio.findById(index).isPresent()){
+            repositorio.deleteById(index);
+            return"Se elimina el movimiento exitosamente";
+
+        }
+        else{
+            return"No se elimino el movimiento";
+        }
+
     }
 
-    public MovimientoDinero actualizarMovimientos(long index, Map<Object,Object> p) {
-        lista.set(index, p);
-        return "Producto modificado";
+    public MovimientoDinero actualizarMovimientos(int index, Map<Object,Object> p) {
+        MovimientoDinero mov = repositorio.findById(index).get();
+        p.forEach((key,value)->{
+            Field campo= ReflectionUtils.findField(MovimientoDinero.class,(String)key);
+            campo.setAccessible(true);
+            ReflectionUtils.setField(campo,mov,value);
+        });
+        return repositorio.save(mov);
+
 
     }
 }
